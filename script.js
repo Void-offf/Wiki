864ec9ff413d47cfc92dd461c1e85bc5fae3a7f5
Void-offf <yanis.maghnouj@sfxdeux.be>
2026-03-09 10:47:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let pages = JSON.parse(localStorage.getItem("wikiPages")) || {}
+let currentPage = null
 
 renderList()
 
@@ -8,24 +9,17 @@ localStorage.setItem("wikiPages", JSON.stringify(pages))
 
 function createPage(){
 
-document.getElementById("editor").style.display="block"
+let title = prompt("Nom de la page")
 
-}
+if(!title) return
 
-function savePage(){
-
-let title = document.getElementById("title").value
-let content = document.getElementById("content").value
-
-pages[title] = content
+pages[title] = ""
 
 saveStorage()
 
 renderList()
 
 showPage(title)
-
-document.getElementById("editor").style.display="none"
 
 }
 
@@ -37,11 +31,11 @@ list.innerHTML=""
 
 for(let title in pages){
 
-let link = document.createElement("a")
+let link=document.createElement("a")
 
-link.innerText = title
+link.innerText=title
 
-link.onclick = ()=> showPage(title)
+link.onclick=()=>showPage(title)
 
 list.appendChild(link)
 
@@ -51,9 +45,61 @@ list.appendChild(link)
 
 function showPage(title){
 
-document.getElementById("pageTitle").innerText = title
+currentPage = title
 
-document.getElementById("pageContent").innerText = pages[title]
+document.getElementById("pageTitle").innerText=title
+
+document.getElementById("editor").innerHTML=pages[title]
+
+}
+
+function savePage(){
+
+if(!currentPage) return
+
+pages[currentPage] = document.getElementById("editor").innerHTML
+
+saveStorage()
+
+alert("Page sauvegardée")
+
+}
+
+function format(command){
+
+document.execCommand(command,false,null)
+
+}
+
+function changeSize(size){
+
+document.execCommand("fontSize",false,size)
+
+}
+
+function addLink(){
+
+let url = prompt("URL du lien")
+
+document.execCommand("createLink",false,url)
+
+}
+
+function addImage(){
+
+let url = prompt("URL de l'image")
+
+document.execCommand("insertImage",false,url)
+
+}
+
+function addVideo(){
+
+let url = prompt("URL de la vidéo")
+
+let video = `<video controls width="400"><source src="${url}"></video>`
+
+document.execCommand("insertHTML",false,video)
 
 }
 
