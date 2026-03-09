d8267852f160bf2eda662573e5ae2171144edd95
Void-offf <yanis.maghnouj@sfxdeux.be>
2026-03-09 10:45:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
-let pages = {}
+let pages = JSON.parse(localStorage.getItem("wikiPages")) || {}
+
+renderList()
+
+function saveStorage(){
+localStorage.setItem("wikiPages", JSON.stringify(pages))
+}
 
 function createPage(){
- document.getElementById("editor").style.display="block"
+
+document.getElementById("editor").style.display="block"
+
 }
 
 function savePage(){
 
- let title = document.getElementById("title").value
- let content = document.getElementById("content").value
+let title = document.getElementById("title").value
+let content = document.getElementById("content").value
 
- pages[title] = content
+pages[title] = content
 
- renderPages()
+saveStorage()
+
+renderList()
+
+showPage(title)
+
+document.getElementById("editor").style.display="none"
+
 }
 
-function renderPages(){
+function renderList(){
 
- let container = document.getElementById("pages")
- container.innerHTML = ""
+let list = document.getElementById("pageList")
 
- for(let title in pages){
+list.innerHTML=""
 
-  let div = document.createElement("div")
-  div.innerHTML = "<h2>"+title+"</h2><p>"+pages[title]+"</p>"
+for(let title in pages){
 
-  container.appendChild(div)
- }
+let link = document.createElement("a")
+
+link.innerText = title
+
+link.onclick = ()=> showPage(title)
+
+list.appendChild(link)
+
+}
+
+}
+
+function showPage(title){
+
+document.getElementById("pageTitle").innerText = title
+
+document.getElementById("pageContent").innerText = pages[title]
+
+}
+
+function toggleDark(){
+
+document.body.classList.toggle("dark")
 
 }
